aa4b39576802d90115dea73786fd12313caae6d6
mRzz129 <114788470+mRzz129@users.noreply.github.com>
2023-10-04 11:42:55 +0700

    Add files via upload

---
+++ TugasSesi2.js
@@ -0,0 +1,8 @@
+const firstName = "Mirza";
+const lastName = "Yusuf Humaedy"; 
+const age = 23;
+const isMarried = false;
+
+console.log(`Halo Nama Saya ${firstName} ${lastName}`);
+console.log(`Umur saya ${age} tahun`);
+console.log(isMarried);
